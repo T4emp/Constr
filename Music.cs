2216d4e286646d25da73121384d931ae9db512b8
T4emp <sergey63777@mail.ru>
2022-02-12 01:45:41 +0300

    Добавьте файлы проекта.

---
+++ Music.cs
@@ -0,0 +1,13 @@
+﻿class Music
+{
+    public string name1;
+    public string name2;
+    public Music() { name1 = "Джаз"; name2 = "Прогрессив-джаз"; }
+    public Music(string n1) { name1 = n1; name2 = "Блюграсс, Кантри-поп"; }
+    public Music(string n1, string n2) { name1 = n1; name2 = n2; }
+
+    public void Print()
+    {
+        Console.WriteLine($"Критерий: {name1}, Подкритерий: {name2}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Music music = new Music();
+Music crtmusic = new Music("Фолк - музыка, Джаз, Кантри, Хип-хоп");
+Music hiphopcrtmusic = new Music("Хип-хоп", "Чикано-рэп, Фристайл-рэп, Гангста-рэп");
+
+music.Print();
+crtmusic.Print();
+hiphopcrtmusic.Print();
+
+music.name1 = "Шансон";
+music.name2 = "Классический шансон, Новый шансон";
+music.Print();
